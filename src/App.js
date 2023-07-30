1071f1fe52285dabf7e1bb3b6d51768ad09ccf00
sukrukiymaciRx <sukru.kiymaci@recoverx.com>
2023-07-30 00:20:19 -0700

    add client only ui

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import {BrowserRouter, Routes, Route} from "react-router-dom"
+import Home from "./components/Home"
+import ChatPage from "./components/ChatPage";
+import socketIO from "socket.io-client"
+
+
+const socket = socketIO.connect("http://165.232.134.254")
+function App() {
+  return (
+    <BrowserRouter>
+        <div>
+          <Routes>
+            <Route path="/" element={<Home socket={socket}/>}></Route>
+            <Route path="/chat" element={<ChatPage socket={socket}/>}></Route>
+          </Routes>
+    </div>
+    </BrowserRouter>
+    
+  );
+}
+
+export default App;
---
+++ src/components/ChatPage.js
@@ -0,0 +1,38 @@
+import React, { useEffect, useState, useRef } from "react";
+import ChatBody from "./ChatBody";
+import ChatFooter from "./ChatFooter";
+import CenterImage from "./CenterImage";
+
+const ChatPage = ({ socket }) => {
+  const [messages, setMessages] = useState([]);
+  const [typingStatus, setTypingStatus] = useState("");
+  const lastMessageRef = useRef(null);
+
+  useEffect(() => {
+    socket.on("messageResponse", (data) => setMessages([...messages, data]));
+  }, [socket, messages]);
+
+  // useEffect(() => {
+  //   socket.on("typingResponse", (data) => setTypingStatus(data));
+  // }, [socket]);
+
+  useEffect(() => {
+    lastMessageRef.current?.scrollIntoView({ behavior: "smooth" });
+  }, [messages]);
+
+  return (
+    <div className="chat">
+      <div className="chat__main">
+        <ChatBody
+          messages={messages}
+          typingStatus={typingStatus}
+          lastMessageRef={lastMessageRef}
+        />
+        <ChatFooter socket={socket} />
+      </div>
+      <CenterImage messages={messages} lastMessageRef={lastMessageRef}/>
+    </div>
+  );
+};
+
+export default ChatPage;
